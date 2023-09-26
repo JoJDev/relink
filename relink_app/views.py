2fbd90133d015ee7088efe898d107ab55dd528ae
JoJDev <jojdeveloper@gmail.com>
2023-09-26 17:47:45 -0600

    Prototype done, ToDo: a better interface

--- relink_app/views.py
+++ relink_app/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Link
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola mundo")
+    return render(request, "relink_app/index.html")
+
+
+def relink(request, slug_url: str = ""):
+    link = get_object_or_404(Link, slug=slug_url)
+    return redirect(link.url)
