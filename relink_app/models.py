ca3ee24f6f5f048b440bd88243f3f261de816686
JoJDev <jojdeveloper@gmail.com>
2023-09-26 16:49:38 -0600

    making link`s model

--- relink_app/models.py
+++ relink_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class Link(models.Model):
+    name = models.CharField(max_length=50, default="title")
+    slug = models.SlugField(default="")
+    url = models.URLField(max_length=200, default="")
+    pub_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+    mod_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    expire_date = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.datetime.now())
+    public = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+
--- relink_proj/settings.py
+++ relink_proj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + ["relink_app"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
