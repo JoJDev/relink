4d3b0f022c07dfe58217b8a15a320e8ca93731da
JoJDev <jojdeveloper@gmail.com>
2023-10-07 22:11:00 -0600

    added new fields to the model and some UI changes

--- relink_app/models.py
+++ relink_app/models.py
@@ -16,6 +16,21 @@ class Link(models.Model):
         auto_now=False, auto_now_add=False, null=True, default=None, blank=True
     )
     public = models.BooleanField(default=True)
+    auto_redirect = models.BooleanField(default=False)
+    seconds_wait = models.IntegerField(default=0)
+    url_broken = models.SmallIntegerField(default=0)
+
+    """
+from relink_app.models import Link
+l = Link.objects.all() 
+    """
 
     def __str__(self):
-        return self.name
+        url_no_scheme: str = self.url[self.url.find("://")+3 :]
+        return (
+            self.name + ": " + (url_no_scheme
+            if len(url_no_scheme) < 10
+            else url_no_scheme[:10])
+        )
+
+        
